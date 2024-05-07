04c326fae8428ec9787151d6b6d9c96e689854c5
Mark <markkamamia@gmail.com>
2024-05-07 23:11:01 +0300

    initial commit

---
+++ src/Components/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+const Navbar = () => {
+  return (
+  
+    <nav className="navbar fixed-top bg-body-tertiary ">
+      <div className="logo">
+
+        <h1>SPICESAFARI RECIPE</h1>
+        
+        </div>
+      <div className="search">
+        <FontAwesomeIcon icon={faSearch} />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
