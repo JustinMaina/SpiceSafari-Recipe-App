013f1107735b45a2faccb999915a4c59ecfe2a4f
JustinMaina <justinmwangi58@gmail.com>
2024-05-09 12:58:29 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,25 +1,32 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Search from './components/Search';
+import RecipeDescription from './components/Description';
 
-// const router = createBrowserRouter([
-  
-//   {
-//     path: '/:id',
-//     element: <App />,
-//   }
-
-// ]);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/search',
+    element: <Search />,
+  },
+  {
+    path: '/description/:id',
+    element: <RecipeDescription />
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <RouterProvider router={router} /> */}
-    <App />
+    
+    <RouterProvider router={router} />
 
   </React.StrictMode>
 );
